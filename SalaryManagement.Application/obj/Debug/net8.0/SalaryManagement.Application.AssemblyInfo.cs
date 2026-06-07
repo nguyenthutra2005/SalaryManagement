ecaa1f214a7affca44310414239f85ad45f11d29
Nguyen Thu Tra <nguyenthutra2509@gmail.com>
2026-06-07 08:21:37 +0700

    Fix: ha cap EF Core packages xuong 8.0.8

--- SalaryManagement.Application/obj/Debug/net8.0/SalaryManagement.Application.AssemblyInfo.cs
+++ SalaryManagement.Application/obj/Debug/net8.0/SalaryManagement.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalaryManagement.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c68b013fd31d9cab1896f034d1a9617726da429")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25693cb05d40677169a40da8baa6efd281440042")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalaryManagement.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalaryManagement.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
